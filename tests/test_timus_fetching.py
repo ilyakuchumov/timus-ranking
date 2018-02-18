88abaefdf540750e01aa6f04f2a2968f3eeb604c
Ilya Kuchumov <ilya.kuchumov@gmail.com>
2018-02-19 01:11:34 +0500

    Add fetching tests

---
+++ tests/test_timus_fetching.py
@@ -0,0 +1,79 @@
+import pytest
+from datetime import datetime
+
+import lib.timus_fetching as timus_fetching
+
+@pytest.mark.parametrize(
+    'time,date,expected',
+    (
+        ('23:04:16', '18 Feb 2018', datetime(2018, 2, 18, 23, 4, 16)),
+        ('23:10:08', '4 Feb 2018', datetime(2018, 2, 4, 23, 10, 8)),
+        ('08:35:33', '18 Feb 2018', datetime(2018, 2, 18, 8, 35, 33)),
+    )
+)
+def test_parsing_date(time, date, expected):
+    actual = timus_fetching.parse_timus_time(time, date)
+    assert actual == expected
+
+
+def test_ce_submit_parsing():
+    html = open('tests/data/ce.html').read()
+    actual = timus_fetching.parse_submit_records(html)
+    expected = [
+        timus_fetching.SubmitRecord(
+            7764902,
+            datetime(2018, 2, 18, 3, 10, 45),
+            243756,
+            1104,
+            'Visual C++ 2017',
+            'Compilation error',
+            0,
+            0,
+            0
+        )
+    ]
+    assert actual == expected
+
+
+def test_ac_submit_parsing():
+    html = open('tests/data/ac.html').read()
+    actual = timus_fetching.parse_submit_records(html)
+    expected = [
+        timus_fetching.SubmitRecord(
+            7764900,
+            datetime(2018, 2, 18, 3, 6, 17),
+            243756,
+            1104,
+            'Visual C++ 2017',
+            'Accepted',
+            0,
+            0.187,
+            260
+        )
+    ]
+    assert actual == expected
+
+
+def test_wa_submit_parsing():
+    html = open('tests/data/wa.html').read()
+    actual = timus_fetching.parse_submit_records(html)
+    expected = [
+        timus_fetching.SubmitRecord(
+            7764901,
+            datetime(2018, 2, 18, 3, 9, 7),
+            243756,
+            1104,
+            'Visual C++ 2017',
+            'Wrong answer',
+            16,
+            0.187,
+            264
+        )
+    ]
+    assert actual == expected
+
+
+def test_1000_submits():
+    html = open('tests/data/1000_submits.html').read()
+    actual = timus_fetching.parse_submit_records(html)
+    assert len(actual) == 1000
